cb4f6ff7a42f903aa3160f244f63ecd35e9380b8
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-02-22 17:01:13 +0700

    Create arkademy().js

---
+++ isengNgikut/arkademy().js
@@ -0,0 +1,39 @@
+/*Buatlah sebuah fungsi yang menampilkan output dari 1 sampai n, dengan
+ketentuan untuk setiap angka berkelipatan 3 akan digantikan dengan kata
+“Arka”, untuk setiap angka berkelipatan 7 akan digantikan dengan kata
+“Demy”, dan untuk setiap angka berkelipatan 3 dan 7 akan digantikan
+dengan kata “Arkademy”. Catatan: Input harus merupakan angka, jika
+bukan angka akan menampilkan error “Input harus angka!”*/
+
+	arkademy('30');
+
+		function arkademy(jumlahx){
+
+			let _array = [];
+			let output = '';
+
+			if (isNaN(jumlahx)==false) {
+				console.log('ini bukan angka');
+				return false
+
+			}
+			
+			for (let i = 0; i < jumlahx; i++) {
+
+				let jumlah = i+1;
+
+				if (jumlah%3==0 && jumlah%7==0) {
+				_array.push('arkademy');
+				}else if(jumlah%3==0){
+					_array.push('arka')
+				}else if(jumlah%7==0){
+					_array.push('demy')
+				}else{
+					_array.push(jumlah);
+				}
+
+			}
+
+			console.log(_array);
+
+		}
